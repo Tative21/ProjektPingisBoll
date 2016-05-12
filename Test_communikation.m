22e5b60ebedcd26537fd80f84d7748a5b5e278d0
Tative21 <William_91@hotmail.se>
2016-05-13 00:14:10 +0200

    Matlab fil
    Matlab fil

---
+++ Test_communikation.m
@@ -0,0 +1,71 @@
+function [] = Test_communikation(V,Kp,Ti,Td,dT)
+s = serial('COM22', 'BaudRate', 115200,'DataBits',8);
+fopen(s);
+%pause(0.5);
+
+flushinput(s);
+
+
+N=100;
+
+V=30;
+Kp=72;
+Ti=141;
+Td=35;
+dT=10;
+Start=1;
+
+PWMvalue=zeros(1,N);
+errorValue=zeros(1,N);
+Distance=zeros(1,N);
+tid =1:N;
+%pause(0.2);
+allValues=[uint8(Start),uint8(V),uint8(Kp),uint8(Ti),uint8(Td),uint8(dT)];
+
+
+%pause(0.2);
+for p = 1:6
+flushinput(s);
+flushoutput(s);
+fwrite(s,allValues(p),'uint8');
+flushinput(s);
+flushoutput(s);
+pause(0.5);
+end
+
+pause(0.2);
+for k=1:N
+
+flushinput(s);
+flushoutput(s);
+
+fwrite(s,'1')
+PWMvalue(k)=str2double(fscanf(s,'%c'));
+
+flushinput(s);
+flushoutput(s);
+
+fwrite(s,'2')
+errorValue(k)=str2double(fscanf(s,'%c'));
+flushinput(s);
+flushoutput(s);
+
+fwrite(s,'3')
+Distance(k)=str2double(fscanf(s,'%c'));
+flushinput(s);
+flushoutput(s);
+
+pause(0.2);
+
+plot(tid,PWMvalue,'k',tid,errorValue,'r-',tid,Distance,'g')
+axis([0,100,-1200,1200]);
+ xlabel('samples')
+ ylabel('Utsignal')
+ title('Pingis-Projekt')
+ legend('PWMvalue','ErrorValue','Distance')
+
+ end
+ fclose(instrfind);
+ delete(instrfind);
+end
+ 
